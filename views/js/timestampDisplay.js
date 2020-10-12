1243717c47ff90382e6db6afb55eb3a9f1a153a7
Alex Kozlov <akoz002@aucklanduni.ac.nz>
2020-10-12 18:47:26 +1300

    Added React component for displaying timestamp

---
+++ views/js/timestampDisplay.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/*
+ * A simple React component to fetch and display the timestamp.
+ */
+
+class TimestampDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      timestamp: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // input field 'onChange' handler
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  // button 'onClick' handler
+  handleClick() {
+    // fetch data from the API
+    fetch('api/timestamp/' + this.state.input.trim())
+      .then(res => res.json())
+      .then(json => this.setState({
+        timestamp: JSON.stringify(json, null, 1),
+        input: ''
+      }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Demo:</h3>
+        <input type="text" value={this.state.input}
+          onChange={this.handleChange}
+          placeholder="Enter date string..."
+        />
+        <button onClick={this.handleClick}>Get Timestamp</button>
+        <p><code>{this.state.timestamp}</code></p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <TimestampDisplay />,
+  document.getElementById('timestamp-display')
+);
