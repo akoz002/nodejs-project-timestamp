3c882dfbc3f0e81c8f08b0336bb54cbd53d2664d
Alex Kozlov <akoz002@aucklanduni.ac.nz>
2020-12-06 17:48:13 +1300

    Updated styling

--- views/src/components/TimestampDisplay.js
+++ views/src/components/TimestampDisplay.js
@@ -10,11 +10,12 @@ export default class TimestampDisplay extends React.Component {
     super(props);
     this.state = {
       input: '',
+      fetching: false,
       timestamp: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    this.getTimestamp = this.getTimestamp.bind(this);
   }
 
   // input field 'onChange' handler
@@ -24,29 +25,74 @@ export default class TimestampDisplay extends React.Component {
     });
   }
 
-  // button 'onClick' handler
-  handleClick() {
+  // 'onSubmit' handler
+  getTimestamp(e) {
+    e.preventDefault();
+
+    const dateString = this.state.input.trim();
+
+    this.setState({
+      input: '',
+      fetching: true,
+      timestamp: ''
+    });
+
     // fetch data from the API
-    fetch('api/timestamp/' + this.state.input.trim())
+    fetch('api/timestamp/' + dateString)
       .then(res => res.json())
       .then(json => this.setState({
-        timestamp: JSON.stringify(json, null, 1),
-        input: ''
+        fetching: false,
+        timestamp: json
       }))
       .catch(err => console.error(err));
   }
 
+  // formats output value with quotes and/or comma
+  formatValue(key, index, length) {
+    const value = this.state.timestamp[key];
+    const result = typeof value === "string" ? `"${value}"` : `${value}`;
+    return index < length - 1 ? result + ',' : result;
+  }
+
   render() {
+    let timestampResult = null;
+    if (this.state.fetching) {
+      timestampResult = (
+        <p className='code-block'>
+          <code>Fetching timestamp...</code>
+        </p>
+      );
+    }
+    else if (this.state.timestamp) {
+      timestampResult = (
+        <ul className='code-block'>
+          <code>
+            <li>{'{'}</li>
+            <ul>
+              {Object.keys(this.state.timestamp).map((key, index, array) =>
+                <li key={key}>
+                  "{key}": {this.formatValue(key, index, array.length)}
+                </li>
+              )}
+            </ul>
+            <li>{'}'}</li>
+          </code>
+        </ul>
+      );
+    }
+
     return (
-      <div>
-        <h3>Demo:</h3>
-        <input type="text" value={this.state.input}
-          onChange={this.handleChange}
-          placeholder="Enter date string..."
-        />
-        <button onClick={this.handleClick}>Get Timestamp</button>
-        <p><code>{this.state.timestamp}</code></p>
-      </div>
+      <form onSubmit={this.getTimestamp}>
+        <div className='input-container'>
+          <label for="date-string">Date string:</label>
+          <input id="date-string" type="text" value={this.state.input}
+            onChange={this.handleChange}
+            placeholder="Enter date string..."
+          />
+          <input type="submit" value="Get Timestamp" />
+        </div>
+        {timestampResult}
+      </form>
     );
   }
 }
